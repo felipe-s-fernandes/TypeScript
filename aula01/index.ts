4126c6d714b6118c72133e0f0c56446a540eca0f
Felipe Fernandes <felipe.bravil@gmail.com>
2023-03-13 11:37:42 -0300

    First commit: 01 - learning basic typing

---
+++ aula01/index.ts
@@ -0,0 +1,27 @@
+type MyUnionType = number | string | boolean | void | null | undefined;
+
+class Validator {
+    private _data: MyUnionType;
+
+    constructor(argument: MyUnionType) {
+        this._data = argument;
+    }
+
+    get data() {
+        return this._data;
+    }
+}
+
+const myNumber = new Validator(123);
+const myString = new Validator("string");
+const myBoolean = new Validator(true);
+const myVoid = new Validator((() => {})());
+const myNull = new Validator(null);
+const myUndefined = new Validator(undefined);
+
+console.log("myNumber: ", myNumber.data, typeof myNumber.data);
+console.log("myString: ", myString.data, typeof myString.data);
+console.log("myBoolean: ", myBoolean.data, typeof myBoolean.data);
+console.log("myVoid: ", myVoid.data, typeof myVoid.data);
+console.log("myNull: ", myNull.data, typeof myNull.data);
+console.log("myUndefined: ", myUndefined.data, typeof myUndefined.data);
